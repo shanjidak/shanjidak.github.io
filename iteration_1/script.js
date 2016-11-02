437b4fc294d64d4a92e837ba87f2d27ae1b24632
shanjidak <kibrs109@newschool.edu>
2016-11-02 12:16:12 -0400

    iteration_1

---
+++ iteration_1/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	//append new element
+	for (i = 0; i < 108; i++) {
+		$("body").append( "<img class='element' src='transform:rotate(" + i * 100 + "deg);'></imgv>");
+		console.log("element " + i + " added");
+    	$("body").append( "<div class='element' style='background-color: hsl(" + i * 3.6 + ", 100%, 50%);'></div>" );
+      	console.log("element " + i + " added");
+	}
+
+
+	//mouse interation
+	$('.element').mouseover(function(){
+		$(this).css('background-color','white');
+	})
+	$('.element').mouseout(function(){
+		$(this).css('background-color','hsl(" + i * 3.6 + ", 100%, 50%)');
+	})
+})
