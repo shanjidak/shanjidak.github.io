a23305ea2314b84fdd1b024172e9e4c261fbaf4d
shanjidak <kibrs109@newschool.edu>
2016-12-14 01:30:36 -0500

    everything

--- iteration_1/script.js
+++ iteration_1/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	//append new element
 	for (i = 0; i < 100; i++) {
-		$("body").append( "<div class='element' src='transform:rotate(" + i * 10 + ", 100%, 50%);'></div>");
+		$("body").append( "<div class='element' style='left:" + "vw; top:" + "vh;'></div>");
 		console.log("element " + i + " added");
     	$("body").append( "<div class='element' style='background-color: hsl(" + i * 3.6 + ", 100%, 50%);'></div>" );
       	console.log("element " + i + " added");
