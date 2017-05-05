29da23ce70d2812ad7328db0ba87c3014520885f
shanjidak <kibrs109@newschool.edu>
2017-05-05 10:55:52 -0400

    final

---
+++ final_project/assets/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("expand").addEventListener("click", function() {
+    this.classList.toggle("is-active");
+});
+
+document.getElementById("title").addEventListener("click", function() {
+    this.classList.toggle("is-active");
+});
+
+document.getElementById("writing").addEventListener("click", function() {
+    this.classList.toggle("is-active");
+});
