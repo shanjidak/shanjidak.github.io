f879069d9a21412e3cf8dfcd500b9404734bc49e
shanjidak <kibrs109@newschool.edu>
2017-05-08 13:13:13 -0400

    progress

--- final_project/assets/js/script.js
+++ final_project/assets/js/script.js
@@ -1,11 +1,35 @@
-document.getElementById("expand").addEventListener("click", function() {
+document.getElementById("expand-1").addEventListener("click", function() {
     this.classList.toggle("is-active");
 });
 
-document.getElementById("title").addEventListener("click", function() {
+document.getElementById("expand-2").addEventListener("click", function() {
     this.classList.toggle("is-active");
 });
 
-document.getElementById("writing").addEventListener("click", function() {
+document.getElementById("expand-3").addEventListener("click", function() {
+    this.classList.toggle("is-active");
+});
+
+document.getElementById("expand-4").addEventListener("click", function() {
+    this.classList.toggle("is-active");
+});
+
+document.getElementById("expand-5").addEventListener("click", function() {
+    this.classList.toggle("is-active");
+});
+
+document.getElementById("expand-6").addEventListener("click", function() {
+    this.classList.toggle("is-active");
+});
+
+document.getElementById("expand-7").addEventListener("click", function() {
+    this.classList.toggle("is-active");
+});
+
+document.getElementById("expand-8").addEventListener("click", function() {
+    this.classList.toggle("is-active");
+});
+
+document.getElementById("full-posts").addEventListener("click", function() {
     this.classList.toggle("is-active");
 });
